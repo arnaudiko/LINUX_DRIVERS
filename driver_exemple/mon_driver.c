41eee4922f79f5c29240304132ec6f9aa2c2e79b
A.mercier <arnaud.iko333@gmail.com>
2020-12-12 19:05:48 +0100

    automatic device creation

--- driver_exemple/mon_driver.c
+++ driver_exemple/mon_driver.c
@@ -5,6 +5,8 @@
 
 // file system and drivers
 #include <linux/fs.h>
+#include <linux/kdev_t.h>
+#include <linux/cdev.h>
 
 // Memory management
 #include <linux/slab.h>
@@ -13,22 +15,24 @@
 //driver interface
 #include "mon_driver.h"
 
-MODULE_LICENSE("none");
-MODULE_SUPPORTED_DEVICE("none");
+// Module informations
+MODULE_LICENSE("GPL");
+MODULE_SUPPORTED_DEVICE(DEVICE_NAME);
 MODULE_AUTHOR("Arnaud MERCIER");
 MODULE_DESCRIPTION("Exemple de driver simple");
 MODULE_VERSION("0.1");
 
-static int major = 145;
-static int minor = 1;
-
+// Module params
 int32_t value = 0;
+module_param(value, int, 0);
+MODULE_PARM_DESC(value, "param test");
 
-module_param(major, int, 0);
-MODULE_PARM_DESC(major, "major number for the driver (default 254)");
+// device
+dev_t dev = 0;
+static struct class* dev_class;
+static struct cdev etx_cdev;
 
-
-/* fonctions de gestion d'un fichier */
+// File device functions
 static int driver_open(struct inode* inode, struct file* file)
 {
 	printk(KERN_DEBUG "Open() \n");
@@ -53,7 +57,7 @@ static ssize_t driver_write(struct file* file, const char* buf, size_t count, lo
 	return 0;
 }
 
-//static int driver_ioctl(struct inode* inode, struct file* file, unsigned int cmd, unsigned long arg)
+
 static long driver_ioctl(struct file* file, unsigned int cmd, unsigned long arg)
 {
 	int ret = 0;
@@ -80,7 +84,7 @@ static long driver_ioctl(struct file* file, unsigned int cmd, unsigned long arg)
 }
 
 
-/* Enregistrement de driver */
+// driver file operations
 static struct file_operations fops = 
 {
 	.owner = THIS_MODULE,
@@ -91,31 +95,61 @@ static struct file_operations fops =
 	.unlocked_ioctl = driver_ioctl
 };
 
-
-int driver_init(void)
+// Modules functions
+int mydriver_init(void)
 {
-	int ret;
-	printk(KERN_INFO "Mon driver init \n");
+	printk(KERN_INFO "Driver init with param = %d\n", value);
 	
-	ret = register_chrdev(major, "mydriver", &fops);
-	if(ret < 0)
+	// Allocating Major and minor numbers
+	if( alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME) < 0)
 	{
-		printk(KERN_WARNING "Probleme lors du chargement du driver (major = %d)\n", major);
-		return ret;
+		printk(KERN_WARNING "Cannot allocate major number\n");
+		return -1;
+	}
+	printk(KERN_INFO "Major = %d and  Minor = %d \n", MAJOR(dev), MINOR(dev));
+	
+	// Creating cdev structure
+	cdev_init(&etx_cdev, &fops);
+	
+	// adding character device to the system
+	if( cdev_add(&etx_cdev, dev, 1) < 0)
+	{
+		printk(KERN_WARNING "Cannot add the device to the system\n");
+		unregister_chrdev_region(dev, 1);
+		return -2;
 	}
 	
-	printk(KERN_DEBUG "mydriver loaded with major %d \n", major);
+	// Creating struct class
+	dev_class = class_create(THIS_MODULE, DEVICE_NAME);
+	if( dev_class == NULL)
+	{
+		printk(KERN_WARNING "Cannot create the struct class\n");
+		unregister_chrdev_region(dev, 1);
+		return -3;
+	}
+	
+	// creating device
+	if( device_create(dev_class, NULL, dev, NULL, DEVICE_NAME) == NULL)
+	{
+		printk(KERN_WARNING "Cannot create the device\n");
+		class_destroy(dev_class);
+		return -4;
+	}
+	
+	printk(KERN_INFO "Device /dev/%s created\n", DEVICE_NAME);
 	return 0;
-
 }
 
-void driver_exit(void)
+void mydriver_exit(void)
 {
-
 	printk(KERN_INFO "Mon driver exit \n");
-	unregister_chrdev(major, "mydriver");
+
+	device_destroy(dev_class, dev);
+	class_destroy(dev_class);
+	cdev_del(&etx_cdev);
+	unregister_chrdev_region(dev, 1);
 }
 
-module_init(driver_init);
-module_exit(driver_exit);
+module_init(mydriver_init);
+module_exit(mydriver_exit);
 
--- driver_exemple/mon_driver.h
+++ driver_exemple/mon_driver.h
@@ -4,6 +4,9 @@
 	#include <sys/ioctl.h>
 #endif
 
+
+#define DEVICE_NAME "mydriver"
+
 #define SAMPLE_IOC_MAGIC 'q'
 #define GETCMD _IOR(SAMPLE_IOC_MAGIC, 0, int32_t*)
 #define SETCMD _IOW(SAMPLE_IOC_MAGIC, 1, int32_t*)
